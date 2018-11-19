31355f6fc31af28fa63789a6f87da9c613d77be7
paprikaLang <3060866223@qq.com>
2018-11-19 20:36:48 +0800

    database save

--- lib/chat_web/channels/winter_warmer_channel.ex
+++ lib/chat_web/channels/winter_warmer_channel.ex
@@ -1,29 +1,28 @@
 defmodule ChatWeb.WinterWarmerChannel do
   use ChatWeb, :channel
 
-  def join("winter_warmer:lobby", payload, socket) do
-    if authorized?(payload) do
-      {:ok, socket}
-    else
-      {:error, %{reason: "unauthorized"}}
-    end
+  alias Chat.Chats
+
+  def join("winter_warmer:lobby", _payload, socket) do
+    {:ok, socket}
   end
 
   # Channels can be used in a request/response fashion
   # by sending replies to requests from the client
-  def handle_in("ping", payload, socket) do
-    {:reply, {:ok, payload}, socket}
-  end
+  # def handle_in("ping", payload, socket) do
+  #   {:reply, {:ok, payload}, socket}
+  # end
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (winter_warmer:lobby).
   def handle_in("shout", payload, socket) do
+    Chats.create_message(payload)
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
 
   # Add authorization logic here as required.
-  defp authorized?(_payload) do
-    true
-  end
+  # defp authorized?(_payload) do
+  #   true
+  # end
 end
